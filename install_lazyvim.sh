f120f7624fb55cdafaa8d5d0b4e549692e4c3f63
Wenbo Pan <pixelwenbo@gmail.com>
2025-03-18 16:41:07 +0800

    Add intall lazyvim

---
+++ install_lazyvim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf ~/.config/nvim
+rm -rf ~/.local/share/nvim
+rm -rf ~/.local/state/nvim
+rm -rf ~/.cache/nvim
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
+nvim

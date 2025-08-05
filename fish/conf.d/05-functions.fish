790e5b0fcc4615f36aea55b479c2bc5ae329f31b
bmpixel <pixelwenbo@gmail.com>
2025-08-05 15:33:49 +0800

    Only match prefix for timer blacklist

--- fish/conf.d/05-functions.fish
+++ fish/conf.d/05-functions.fish
@@ -19,7 +19,7 @@ set -g CMD_START_TIME 0
 set -g CMD_NOTIFICATION_THRESHOLD 180  # 3 minutes in seconds
 
 # Blacklist of interactive commands that should not trigger notifications
-set -g CMD_TIMER_BLACKLIST vim nvim vi less more man htop top nano emacs ssh tmux screen git fzf ranger mc mutt weechat irssi node ipython fish bash zsh mysql psql mongo redis-cli claude tx
+set -g CMD_TIMER_BLACKLIST vim nvim vi less more man htop top nano emacs ssh tmux screen git fzf ranger mc mutt weechat irssi node ipython fish bash zsh mysql psql mongo redis-cli claude tx v
 
 function __cmd_timer_start --on-event fish_preexec
     set -g CMD_START_TIME (date +%s)
@@ -43,8 +43,16 @@ function __cmd_timer_end --on-event fish_postexec
             # Extract the first word (command name) to check against blacklist
             set -l cmd_name (string split ' ' $command)[1]
             
-            # Skip notification if command is in blacklist
-            if not contains $cmd_name $CMD_TIMER_BLACKLIST
+            # Skip notification if command starts with a blacklisted word
+            set -l is_blacklisted false
+            for blacklisted in $CMD_TIMER_BLACKLIST
+                if string match -q "$blacklisted*" $cmd_name
+                    set is_blacklisted true
+                    break
+                end
+            end
+            
+            if not $is_blacklisted
                 # Truncate command if too long for notification
                 if test (string length $command) -gt 80
                     set command (string sub -l 77 $command)"..."

cc698feca07407f35bb7d3f1f71367473d2df170
bmpixel <pixelwenbo@gmail.com>
2025-09-24 17:16:03 +0800

    feat: finetune configs

--- fish/conf.d/04-plugins.fish
+++ fish/conf.d/04-plugins.fish
@@ -12,5 +12,6 @@ has uv; and uv generate-shell-completion fish | source
 has uvx; and uvx --generate-shell-completion fish | source
 has vim; and set -x EDITOR "vim"
 has nvim; and set -x EDITOR "nvim"
+has cursor; and set -x EDITOR "cursor"
 has batcat; and alias bat "batcat"
 # has starship; and starship init fish | source
